93fc25d957211c3bdeb38b4310a4f6fd559ad288
Vishal Bardoloi <v.bardoloi@gmail.com>
2018-02-09 03:10:50 -0600

    Moved JS code to its own folder. Added prettier.io and integrated it into the .vscode editor and added a yarn script.

---
+++ js/ClientApp.jsx
@@ -0,0 +1,20 @@
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+};
+
+const MyFirstComponent = function() {
+  return ce(
+    'div',
+    { id: 'my-first-component' },
+    ce(MyTitle, { title: 'Breaking Bad', color: 'YellowGreen' }),
+
+    ce(MyTitle, { title: 'Game of Thrones', color: 'GreenYellow' }),
+    ce(MyTitle, { title: 'Stranger things', color: 'Peru' }),
+    ce(MyTitle, { title: 'Planet Earth 2', color: 'Lime' }),
+    ce(MyTitle, { title: 'The Hunt', color: 'Green' })
+  );
+};
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'));
